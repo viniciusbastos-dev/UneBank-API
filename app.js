4ea5de910a9f9138a93a88900b5e5966c94dbeef
Vinícius Bastos <viniciusbsilva57@gmail.com>
2024-03-30 12:51:23 -0300

    feat - create middleware for handling errors

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
+require("express-async-errors");
 const mongoose = require("mongoose");
 const express = require("express");
+const cors = require("cors");
 const config = require("./utils/config");
 const logger = require("./utils/logger");
 const userRouter = require("./controller/user");
+const { unknownEndpoint, errorHandler } = require("./utils/middleware");
 
 const app = express();
 
@@ -17,8 +20,12 @@ mongoose
     logger.error("Error connecting to MongoDB", error.message);
   });
 
+app.use(cors());
 app.use(express.json());
 
 app.use("/api/user", userRouter);
+
+app.use(unknownEndpoint);
+app.use(errorHandler);
 
 module.exports = app;
